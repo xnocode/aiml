aabc4ce5fca309793983582d70ba3943a39556b8
Ridoy <xridoynocode@gmail.com>
2026-06-13 00:58:46 +0600

    all about print function
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ python/print.py
@@ -0,0 +1,17 @@
+print("Hello World") 
+# Hello World
+
+print("Hello World")
+print("With Python")
+# Hello World
+# With Python
+
+print("Hello World", "With Python") 
+#Hello World With Python
+
+
+print("Hello \n World", "With Python")
+"""
+Hello
+ World With Python
+"""
